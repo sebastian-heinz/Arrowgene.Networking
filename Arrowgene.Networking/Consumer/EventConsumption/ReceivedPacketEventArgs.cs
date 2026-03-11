e00f6cd09add5fb9c587bc0526dc3ce87d5cbbe6
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2026-03-11 09:13:36 +0800

    up

---
+++ Arrowgene.Networking/Consumer/EventConsumption/ReceivedPacketEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Arrowgene.Networking.SAEAServer;
+
+namespace Arrowgene.Networking.Consumer.EventConsumption
+{
+    /// <summary>
+    /// Event payload for a received packet notification.
+    /// </summary>
+    public class ReceivedPacketEventArgs : EventArgs
+    {
+        internal ReceivedPacketEventArgs(ClientHandle socket, byte[] data)
+        {
+            Socket = socket;
+            Data = data;
+        }
+
+        /// <summary>
+        /// Gets the client that produced the packet.
+        /// </summary>
+        public ClientHandle Socket { get; }
+
+        /// <summary>
+        /// Gets the copied packet payload.
+        /// </summary>
+        public byte[] Data { get; }
+    }
+}
